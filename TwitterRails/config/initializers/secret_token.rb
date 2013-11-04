910cf2530d563ba82e81a5e6ae9210aa19c5e17f
Garrett A. Enquist <garrett.enquist@gmail.com>
2013-11-04 10:27:25 -0600

    first commit

---
+++ TwitterRails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterRails::Application.config.secret_token = '1b12a6eebc0c00e9376458b7f32e46c7532e6cb26c6626421c9ec5069a55bd52d4fad852b57fa9e22b9c9a515b3947b7355c129e8280a704a9bc3081ec779911'
